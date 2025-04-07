116a9ae6c970a4b5c365b0d06074e46e1c565d26
Shivam yadav <123585347+shivamyadav48@users.noreply.github.com>
2025-04-07 13:15:58 +0530

    Update pref.dart

--- lib/helper/pref.dart
+++ lib/helper/pref.dart
@@ -6,9 +6,7 @@ class Pref {
   static late Box _box;
 
   static Future<void> initialize() async {
-    //for initializing hive to use app directory
-    // Hive.defaultDirectory = (await getApplicationDocumentsDirectory()).path;
-    // _box = Hive.box(name: 'myData');
+   
 
     await Hive.initFlutter();
     _box = await Hive.openBox('myData');
@@ -18,21 +16,7 @@ class Pref {
       _box.get('showOnboarding', defaultValue: true);
   static set showOnboarding(bool v) => _box.put('showOnboarding', v);
 
-  // Normal Way - Get
-  // how to call
-  // showOnboarding()
-
-  // static bool showOnboarding() {
-  //   return _box.get('showOnboarding', defaultValue: true);
-  // }
-
-  // Normal Way - Set
-  // how to call
-  // showOnboarding(false)
-
-  // static bool showOnboarding(bool v) {
-  //   _box.put('showOnboarding', v);
-  // }
+ 
 
   //for storing theme data
   static bool get isDarkMode => _box.get('isDarkMode') ?? false;
